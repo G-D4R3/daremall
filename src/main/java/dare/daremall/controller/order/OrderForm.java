9a65e9b29a50eb0f2dd2baf60fd2ec4a7342f79c
dare <gdare1999@gmail.com>
2022-05-10 15:46:01 +0900

    validation 추가

--- src/main/java/dare/daremall/controller/order/OrderForm.java
+++ src/main/java/dare/daremall/controller/order/OrderForm.java
@@ -2,14 +2,21 @@ package dare.daremall.controller.order;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class OrderForm {
 
+    @NotEmpty(message = "수령인 이름를 입력해주세요")
     private String name;
+    @NotEmpty(message = "수령인 연락처를 입력해주세요")
     private String phone;
 
+    @NotEmpty(message = "주소를 입력해주세요")
     private String city;
+    @NotEmpty(message = "주소를 입력해주세요")
     private String street;
+    @NotEmpty(message = "주소를 입력해주세요")
     private String zipcode;
 
 }
