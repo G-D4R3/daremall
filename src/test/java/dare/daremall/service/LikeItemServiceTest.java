37811baf12423520ea5d6a474eb87d6852274b61
dare <gdare1999@gmail.com>
2022-07-02 20:31:27 +0900

    likeItemServiceTest

--- src/test/java/dare/daremall/service/LikeItemServiceTest.java
+++ src/test/java/dare/daremall/service/LikeItemServiceTest.java
@@ -1,16 +1,19 @@
 package dare.daremall.service;
 
+import dare.daremall.controller.item.ItemDto;
 import dare.daremall.controller.member.auth.MemberSignupRequestDto;
 import dare.daremall.domain.LikeItem;
 import dare.daremall.domain.Member;
 import dare.daremall.domain.item.Book;
 import dare.daremall.domain.item.Item;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,13 +21,13 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @Transactional
-class LikeItemServiceTest {
+public class LikeItemServiceTest {
 
     @Autowired ItemService itemService;
     @Autowired MemberService memberService;
 
-    /*@Test
-    void 좋아요() {
+    @Test
+    public void 좋아요() {
         MemberSignupRequestDto memberDto = new MemberSignupRequestDto();
         memberDto.setName("지창민");
         memberDto.setLoginId("jcm115");
@@ -35,27 +38,29 @@ class LikeItemServiceTest {
         memberDto.setDetail("어딘가");
         memberService.join(memberDto);
 
-        Book book = new Book();
+        ItemDto book = new ItemDto();
         book.setName("book1");
         book.setPrice(1000);
         book.setStockQuantity(10);
+        book.setType("B");
         book.setAuthor("author1");
         book.setIsbn("111-111-1111");
-        itemService.saveItem(book);
+        book.setItemStatus("FOR_SALE");
+        long bookId = itemService.saveItem(book);
 
-        Item findItem = itemService.findOne(book.getId());
+        Item findItem = itemService.findOne(bookId);
 
-        memberService.changeLikeItem(memberDto.getLoginId(), findItem.getId());
+        memberService.changeLikeItem(memberDto.getLoginId(), bookId);
 
         Member findMember = memberService.findUser(memberDto.getLoginId());
 
         assertThat(findMember.getLikes().size()).isEqualTo(1);
-        assertThat(findMember.getLikes().get(0).getItem()).isEqualTo(findItem);
+        assertThat(findMember.getLikes().stream().map(li->li.getItem()).collect(Collectors.toList()).contains(findItem)).isTrue();
 
     }
 
     @Test
-    void 좋아요_취소() {
+    public void 좋아요_취소() {
         // given
         MemberSignupRequestDto memberDto = new MemberSignupRequestDto();
         memberDto.setName("지창민");
@@ -67,28 +72,26 @@ class LikeItemServiceTest {
         memberDto.setDetail("어딘가");
         memberService.join(memberDto);
 
-        Book book = new Book();
+        ItemDto book = new ItemDto();
         book.setName("book1");
         book.setPrice(1000);
         book.setStockQuantity(10);
+        book.setType("B");
         book.setAuthor("author1");
         book.setIsbn("111-111-1111");
-        itemService.saveItem(book);
+        book.setItemStatus("FOR_SALE");
+        long bookId = itemService.saveItem(book);
 
-        Item findItem = itemService.findOne(book.getId());
-        memberService.changeLikeItem(memberDto.getLoginId(), findItem.getId());
-
+        Item findItem = itemService.findOne(bookId);
+        memberService.changeLikeItem(memberDto.getLoginId(), bookId); // 좋아요 추가
         Member findMember = memberService.findUser(memberDto.getLoginId());
 
-        assertThat(findMember.getLikes().size()).isEqualTo(1);
-        assertThat(findMember.getLikes().get(0).getItem()).isEqualTo(findItem);
-
         // when
-        memberService.changeLikeItem(findMember.getLoginId(), findItem.getId());
-        findMember = memberService.findUser(findMember.getLoginId());
+        memberService.changeLikeItem(memberDto.getLoginId(), bookId); // 좋아요 취소
 
         // then
         assertThat(findMember.getLikes().size()).isEqualTo(0);
+        assertThat(findMember.getLikes().stream().map(li->li.getItem()).collect(Collectors.toList()).contains(findItem)).isFalse();
 
-    }*/
+    }
 }
